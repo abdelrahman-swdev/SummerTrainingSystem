2d28395314c3114f45682a8cda2ae684d5d65760
AliSamy-developer <aliisamy00@gmail.com>
2022-03-20 02:55:11 +0200

    Create the part of departments, create Department and delete department

---
+++ SummerTrainingSystem/Controllers/DepartmentsController.cs
@@ -0,0 +1,67 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SummerTrainingSystem.Models;
+using SummerTrainingSystemCore.Entities;
+using SummerTrainingSystemCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SummerTrainingSystem.Controllers
+{
+    [Route("department")]
+    public class DepartmentsController : Controller
+    {
+        private readonly IGenericRepository<Department> _depRepo;
+        private readonly IMapper _mapper;
+        private readonly INotyfService _notyfService;
+
+        public DepartmentsController(IGenericRepository<Department> depRepo,
+            IMapper mapper,
+            INotyfService notyfService)
+        {
+            _depRepo = depRepo;
+            _mapper = mapper;
+            _notyfService = notyfService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetDepartments()
+        {
+            var departments = await _depRepo.ListAllAsync();
+            return View(departments);
+        }
+        [HttpGet("new")]
+        public IActionResult CreateDepartment()
+        {
+            return View();
+        }
+        [HttpPost("new")]
+        public IActionResult CreateDepartment(CreateDepartmentVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _depRepo.Add(_mapper.Map<Department>(model));
+                if (result > 0)
+                {
+                    _notyfService.Success("Training created successfully");
+                    return RedirectToAction("GetDepartments");
+                }
+            }
+            return View(model);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDepartment(int id)
+        {
+            try
+            {
+                return _depRepo.Delete(await _depRepo.GetByIdAsync(id)) == 0 ? BadRequest() : Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ SummerTrainingSystem/Models/CreateDepartmentVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SummerTrainingSystem.Models
+{
+    public class CreateDepartmentVM
+    {
+        [Required, MaxLength(256)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Abbreviation { get; set; }
+    }
+}
